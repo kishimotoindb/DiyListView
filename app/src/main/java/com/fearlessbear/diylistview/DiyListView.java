ac48d06ab7da5c83bda71ad07c5e02ced382041f
fearlessBear <519376470@qq.com>
2017-04-05 19:01:14 +0800

    all children are visible, although some of them are not displayed on the scroon. ViewGroup draws children on screen, whose measured height isn't 0. So, we can control displaying for child by measuring them again , so that their measured height become non-zero.

--- app/src/main/java/com/fearlessbear/diylistview/DiyListView.java
+++ app/src/main/java/com/fearlessbear/diylistview/DiyListView.java
@@ -2,11 +2,18 @@ package com.fearlessbear.diylistview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * Created by root on 17-4-5.
@@ -16,6 +23,10 @@ import java.util.List;
  */
 public class DiyListView extends LinearLayout {
     private ArrayList<String> mData;
+    private float mOldY;
+    private float mDy;
+    private float mStartY;
+    private ViewTreeObserver observer;
 
     public DiyListView(Context context) {
         this(context, null);
@@ -31,10 +42,71 @@ public class DiyListView extends LinearLayout {
     }
 
     public void setData(List<String> data) {
-        
+        if (data == null) {
+            return;
+        }
+
+        if (mData == null) {
+            mData = new ArrayList<>();
+        }
+
+        mData.addAll(data);
+        initListView(mData);
     }
 
+    private void initListView(ArrayList<String> mData) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        for (String s : mData) {
+            addView(buildItem(inflater, R.layout.item, s));
+        }
 
+        observer = getViewTreeObserver();
+        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                for (int i = 0; i < getChildCount(); i++) {
+                    Log.i("xiong", i + ": " + getChildAt(i).getMeasuredHeight());
+                }
+                observer.removeOnPreDrawListener(this);
+                return true;
+            }
+        });
+    }
 
+    private View buildItem(LayoutInflater inflater, int layoutRes, final String s) {
+        final View item = inflater.inflate(layoutRes, this, false);
+        ((TextView) item.findViewById(R.id.text)).setText(s);
+        return item;
+    }
 
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final float y = event.getY();
+        final int action = event.getAction();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mOldY = y;
+                mStartY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                onActionMove(0);
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }
+
+    private void onActionMove(float dy) {
+
+    }
 }
--- app/src/main/java/com/fearlessbear/diylistview/MainActivity.java
+++ app/src/main/java/com/fearlessbear/diylistview/MainActivity.java
@@ -3,11 +3,22 @@ package com.fearlessbear.diylistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    static final ArrayList<String> mData = new ArrayList<>();
+
+    {
+        for (int i = 0; i < 50; i++) {
+            mData.add("data is " + i);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DiyListView listView = (DiyListView) findViewById(R.id.listView);
+        listView.setData(mData);
     }
 }
