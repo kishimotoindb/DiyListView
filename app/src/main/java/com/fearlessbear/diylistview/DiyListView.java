c8c143758b021358b0bccc0ce49d91a29c5b53c2
fearlessBear <519376470@qq.com>
2017-04-07 18:39:22 +0800

    。。

--- app/src/main/java/com/fearlessbear/diylistview/DiyListView.java
+++ app/src/main/java/com/fearlessbear/diylistview/DiyListView.java
@@ -23,7 +23,7 @@ import java.util.zip.Inflater;
  * 2.在onTouchEvent中处理滚动事件
  * 2）移动list中的子View。向上移动时，如果
  * 3.complete reuse item
- *
+ * <p>
  * 总结：
  * 1.如果使用循环addView，那么是在循环结束后，才调用layout和draw流程。所以在每个addView执行结束时，什么尺寸参数也
  * 得不到。
@@ -31,14 +31,13 @@ import java.util.zip.Inflater;
  * 04-06 16:30:36.507 31595-31595/com.fearlessbear.diylistview I/xiong: layout is 722
  * 04-06 16:30:36.507 31595-31595/com.fearlessbear.diylistview I/xiong: pre draw is 722
  */
-public class DiyListView extends LinearLayout {
+public class DiyListView extends ViewGroup {
     private ArrayList<String> mData;
     private float mOldY;
     private float mDy;
     private float mStartY;
     private ViewTreeObserver observer;
-    private float mItemHeight;
-    int i;
+    private int mItemTotalHeight = 0;
 
     public DiyListView(Context context) {
         this(context, null);
@@ -50,60 +49,67 @@ public class DiyListView extends LinearLayout {
 
     public DiyListView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setOrientation(VERTICAL);
     }
 
     public void setData(List<String> data) {
-        if (data == null) {
-            return;
-        }
-
         if (mData == null) {
             mData = new ArrayList<>();
         }
 
         mData.addAll(data);
-        initListView(mData);
+        requestLayout();
     }
 
-    private void initListView(ArrayList<String> mData) {
-        ViewTreeObserver observer = getViewTreeObserver();
-        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                Log.i("xiong", "pre draw is " + SystemClock.currentThreadTimeMillis() + "");
-                return true;
-            }
-        });
-        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                Log.i("xiong", "layout is " + SystemClock.currentThreadTimeMillis() + "");
-            }
-        });
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.i("xiong", "list height is " + MeasureSpec.getSize(heightMeasureSpec));
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
 
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-
-        for (final String s : mData) {
-            View child = buildItem(inflater, R.layout.item, s);
-            addView(child);
-            Log.i("xiong", "add " + s + ": " + SystemClock.currentThreadTimeMillis() + "");
-
-            if (mItemHeight == 0) {
-                mItemHeight = child.getMeasuredHeight();
+        if (getChildCount() == 0 && mData != null) {
+            for (int i = 0; i < mData.size(); i++) {
+                View view = getView(mData.get(i));
+                view.measure(widthMeasureSpec, getNewHeightMeasureSpec(heightMeasureSpec, mItemTotalHeight));
+                if (view.getMeasuredHeight() == 0) {
+                    break;
+                } else {
+                    mItemTotalHeight += view.getMeasuredHeight();
+                    addView(view);
+                }
             }
-//            Log.i("xiong", "initListView: item height is " + child.getMeasuredHeight());
-            if (child.getMeasuredHeight() != mItemHeight) {
-                break;
-            }
+        } else {
+
         }
-//        Log.i("xiong", "initListView: item count is " + getChildCount());
     }
 
-    private View buildItem(LayoutInflater inflater, int layoutRes, final String s) {
-        final View item = inflater.inflate(layoutRes, this, false);
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int used = getMeasuredHeight();
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            int childTop = t + used;
+            int childBottom = childTop + child.getMeasuredHeight();
+            child.layout(l, childTop, r, childBottom);
+        }
+    }
+
+    private int getNewHeightMeasureSpec(int heightMeasureSpec, int mItemTotalHeight) {
+        int viewHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int restHeight = viewHeight - mItemTotalHeight;
+        return MeasureSpec.makeMeasureSpec(restHeight, MeasureSpec.getMode(heightMeasureSpec));
+    }
+
+    private View getView(String s) {
+        View item = inflate(getContext(), R.layout.item, null);
         ((TextView) item.findViewById(R.id.text)).setText(s);
         return item;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        Log.i("xiong", "the count of item is " + getChildCount());
     }
 
     @Override
@@ -137,3 +143,38 @@ public class DiyListView extends LinearLayout {
 
     }
 }
+
+//总结1
+    /*private void initListView(ArrayList<String> mData) {
+        ViewTreeObserver observer = getViewTreeObserver();
+        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                Log.i("xiong", "pre draw is " + SystemClock.currentThreadTimeMillis() + "");
+                return true;
+            }
+        });
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Log.i("xiong", "layout is " + SystemClock.currentThreadTimeMillis() + "");
+            }
+        });
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        for (final String s : mData) {
+            View child = buildItem(inflater, R.layout.item, s);
+            addView(child);
+            Log.i("xiong", "add " + s + ": " + SystemClock.currentThreadTimeMillis() + "");
+
+            if (mItemHeight == 0) {
+                mItemHeight = child.getMeasuredHeight();
+            }
+//            Log.i("xiong", "initListView: item height is " + child.getMeasuredHeight());
+            if (child.getMeasuredHeight() != mItemHeight) {
+                break;
+            }
+        }
+//        Log.i("xiong", "initListView: item count is " + getChildCount());
+    }*/
--- app/src/main/java/com/fearlessbear/diylistview/MainActivity.java
+++ app/src/main/java/com/fearlessbear/diylistview/MainActivity.java
@@ -2,6 +2,8 @@ package com.fearlessbear.diylistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DiyListView listView = (DiyListView) findViewById(R.id.listView);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Log.i("xiong", "screen size h*w is " + displayMetrics.heightPixels + " * " + displayMetrics.widthPixels);
         listView.setData(mData);
     }
+
 }
